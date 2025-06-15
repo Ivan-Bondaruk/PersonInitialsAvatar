c0b540f33ce2bcdaae60ca64d8a3d5d7d1d73f89
Ivan Bondaruk <bondarukid@icloud.com>
2025-06-15 16:01:06 +0200

    Update PersonInitialsAvatar.swift
    Fix init

--- Sources/PersonInitialsAvatar/PersonInitialsAvatar.swift
+++ Sources/PersonInitialsAvatar/PersonInitialsAvatar.swift
@@ -6,7 +6,13 @@ import SwiftUI
 public struct PersonInitialsAvatarView: View {
     let fullName: String
     let diameter: CGFloat
-    let backgroundColor: Color? = nil
+    let backgroundColor: Color
+    
+    init(fullName: String, diameter: CGFloat, backgroundColor: Color = .blue) {
+        self.fullName = fullName
+        self.diameter = diameter
+        self.backgroundColor = backgroundColor
+    }
 
     private var initials: String {
         fullName
@@ -18,19 +24,15 @@ public struct PersonInitialsAvatarView: View {
             .uppercased()
     }
 
-    private var resolvedBackgroundColor: Color {
-        backgroundColor ?? .blue
-    }
-
     public var body: some View {
         Text(initials)
             .font(.system(size: diameter / 2.5, weight: .bold))
             .foregroundColor(.white)
             .frame(width: diameter, height: diameter)
-            .background(Circle().fill(resolvedBackgroundColor))
+            .background(Circle().fill(backgroundColor))
     }
 }
 
 #Preview {
-    PersonInitialsAvatarView(fullName: "Ivan Bondaruk", diameter: 100)
+    PersonInitialsAvatarView(fullName: "Ivan Bondaruk", diameter: 100, backgroundColor: .secondary)
 }
